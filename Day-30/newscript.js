32215b3164cbc2b9039ea091fe1987369d8c0d8a
FrontendDev-Basheer <basheermmmofficial@gmail.com>
2023-09-21 08:47:23 +0530

    bs and js

---
+++ Day-30/newscript.js
@@ -0,0 +1,26 @@
+//Output Ways
+document.write("Hello! Everyone");
+console.log("Welcome Everyone");
+window.alert("Welcome to My Website");
+document.getElementById("para").innerHTML="My Website";
+
+//variables
+
+var first_name="Welcome to Javascript";
+console.log(first_name);
+
+//calculations
+var a = 30;
+var b = 20;
+var c = a + b;
+console.log(c);
+
+var c = a - b;
+console.log(c);
+
+var c = a*b;
+console.log(c);
+
+var c = a/b;
+console.log(c);
+window.alert(c);
